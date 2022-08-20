f944f2a99a8c7960f4c6c36e5f4c14aaebe6d701
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-20 08:16:34 +0530

    Update approve.py

--- FallenRobot/modules/approve.py
+++ FallenRobot/modules/approve.py
@@ -225,6 +225,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "𝙰ᴘᴘʀᴏᴠᴇ 🍒"
+__mod_name__ = "Aᴘᴘʀᴏᴠᴇ 🍒"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]

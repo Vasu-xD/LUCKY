89329f9e03d7d88901f994a3d78dc026f81f6a57
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-25 14:14:48 +0530

    Update memify.py

--- FallenRobot/modules/memify.py
+++ FallenRobot/modules/memify.py
@@ -77,7 +77,7 @@ async def drawText(image_path, text):
 
     else:
 
-        fnt = "./FallenRobot/resources/default (1).ttf"
+        fnt = "./FallenRobot/resources/fonts/CROWNT.TTF"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
